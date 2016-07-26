0b916d7a563ad313d2530823b78f860bfce7c5b8
devojoyti <devjyoti.itachi@gmail.com>
2016-07-26 17:01:16 +0530

    Added song download option

--- README.md
+++ README.md
@@ -5,5 +5,4 @@
 Get your latest top 100 latest songs from billboard.com top 100 list, with just a click. A text file will be automatically generated in the same directory of the residing script with top 100 songs.
 
 
-
-### Automatic Download of top 10 songs under going.... 
+ 
--- latest_billboard_songs.py
+++ latest_billboard_songs.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib2
+import selenium
+from selenium import webdriver
 import time
 import os
 
@@ -10,6 +12,8 @@ soup = BeautifulSoup(urllib2.urlopen(url), 'html.parser')
 
 songs = soup.findAll(class_='chart-row__title')
 
+song_names = dict()
+
 try:
         
     print 'writing songs on chart'
@@ -19,13 +23,57 @@ try:
     for s in songs:
         artist = s.contents[3].string[33:]
         file.write(str(count) + '. ' + s.contents[1].string + ' - ' + artist + '\n')
+        if count < 10:
+            for a in range(len(artist)):
+                if artist[a] == ' ':
+                    singer = artist[:a]
+                    print singer
+                    break
+            song_names[s.contents[1].string] = singer
         count +=1
 
     file.close()
     print 'Succesfully written songs on the text file\n'
-    os.system('notepad.exe Billboard Top Songs.txt')
 
 except:
     print 'Cannot open text file for writing the songs!\n'
 
+print 'Do you want to download top 10 songs too ? y/n'
+inp = raw_input()
+if inp == 'y':
 
+    chromedriver = "C:/Python27/Scripts/chromedriver"
+    os.environ["webdriver.chrome.driver"] = chromedriver
+    driver = webdriver.Chrome(chromedriver)
+    count = 1
+    for n in song_names:
+        name = list(n)
+        for i in range(len(name)):
+            if name[i] == ' ':
+                name[i] = '+'
+        try:
+            url = 'http://pleer.net/search?q=' + ''.join(name) + '+' + song_names[n]
+            soup = BeautifulSoup(urllib2.urlopen(url), 'html.parser')
+            song = soup.find(class_='icon download-btn2')
+            song_download_url = 'http://pleer.net' + song.contents[1]['href']
+
+            driver.get(song_download_url)
+            download_btn = driver.find_element_by_class_name('download-btn2')
+            download_btn.click()    #hopefully starts download
+            print 'Download of song %d will start shortly....'%count
+        except:
+            print 'Some error occured during download.....skipping to next song....'
+              
+        count +=1
+        if count > 10:
+            break
+
+    print 'All 10 songs downloaded...'
+    driver.close()
+
+elif inp == 'n':
+    print 'ok, you can see list opened at the side'
+    os.system('notepad.exe Billboard Top Songs.txt')
+else:
+    print 'invalid input, considering it as no anyway...'
+    os.system('notepad.exe Billboard Top Songs.txt')
